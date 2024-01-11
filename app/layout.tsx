c3107ed25739c5ffadc9f036bcee5f82c539817b
Valter Junior <79730793+valtercfjunior@users.noreply.github.com>
2024-01-11 18:04:21 -0300

    metadabase issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     default: 'Next Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://nextjs-dashboard-rouge-delta-77.vercel.app/'),
 };
 
 
